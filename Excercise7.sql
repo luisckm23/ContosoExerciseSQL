bbd4a00e48c887821d15d0e4e5cebac39afd31ab
Luis Juarez <luisckm@gmail.com>
2025-03-28 11:05:18 -0600

    Ejercicio 7 contiene tareas con las funciones Window y su uso

---
+++ Excercise7.sql
@@ -0,0 +1,30 @@
+-- Window FUNCTIONS
+-- Las Window Functions no agrupan registros, sino que realizan cálculos sobre un conjunto de filas relacionado con cada fila individual, 
+-- sin perder detalles de los datos, a diferencia de GROUP BY, que agrupa y reduce el número de filas en el resultado.
+-- Las WINDOW Funtion pueden ser como un FOR, recorren cada fila y hacen el calculo,
+-- mientras un GROUP BY primero agrupa las filas y después hace el calculo
+
+-- en este caso la funcion WINDOW nos sive para particionar el promedio de pago de cada una de las Clasificacion_credito
+select cl.Nombre, cl.Clasificacion_credito, avg(mo.Pago) over(partition by cl.Clasificacion_credito)
+from clientes cl
+join morosos mo
+	on cl.Nombre = mo.Nombre; 
+    
+-- en este caso la funcion WINDOW nos sive para particionar la suma de pago de cada una de las Clasificación_credito
+select cl.Nombre, cl.Clasificacion_credito, sum(mo.Pago) over(partition by cl.Clasificacion_credito)
+from clientes cl
+join morosos mo
+	on cl.Nombre = mo.Nombre; 
+
+-- este query lo usamos con un row_number para asignar un ranking de cada particion que hicimos sobre Clasificacion_credito y 
+-- ordenamos ese ranking por saldo de cada Clasificacion_credito de manera descendente 
+-- tambien agregamos un rank, con el que si existe algun dato con el mismo valor le da el mismo ranking y salta al siguiente valor
+-- numérico; es decir que si tenemos dos valores con 4000, los rankea hipoteticamente como 5 a los dos y el siguiente lugar en el 
+-- ranking será 7
+select cl.Nombre, cl.Clasificacion_credito, saldo,
+row_number() over(partition by cl.Clasificacion_credito order by saldo desc) as row_num,
+rank() over(partition by cl.Clasificacion_credito order by saldo desc) as rrank_num
+from clientes cl
+join morosos mo
+	on cl.Nombre = mo.Nombre; 
+    
